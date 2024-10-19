7fd5de691c1d8bed973ba717150cd487e4127b45
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2024-10-19 14:41:38 -0400

    feat: Creating a new Project

---
+++ src/test/java/br/com/rts/estudos/graphql/security/SecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rts.estudos.graphql.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
