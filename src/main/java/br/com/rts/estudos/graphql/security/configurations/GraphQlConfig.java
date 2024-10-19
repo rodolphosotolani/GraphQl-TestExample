19fb0a633e978269cb8a474004eacbf5735c1723
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2024-10-19 16:04:30 -0400

    feat: Creating Beans configurations to Faker entity and GraphQL Scalars.

---
+++ src/main/java/br/com/rts/estudos/graphql/security/configurations/GraphQlConfig.java
@@ -0,0 +1,24 @@
+package br.com.rts.estudos.graphql.security.configurations;
+
+import graphql.scalars.ExtendedScalars;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.graphql.GraphQlSourceBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+@Log4j2
+@Configuration
+public class GraphQlConfig {
+
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        return wiringBuilder -> wiringBuilder
+                .scalar(ExtendedScalars.DateTime);
+    }
+
+    @Bean
+    GraphQlSourceBuilderCustomizer inspectionCustomizer() {
+        return source -> source.inspectSchemaMappings(report -> log.info(report.toString()));
+    }
+}
