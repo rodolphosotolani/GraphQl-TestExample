0b2f011d7092243ba25290295f33c90ca824e389
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2024-10-19 16:05:37 -0400

    feat: comment unnecessary test

--- src/test/java/br/com/rts/estudos/graphql/security/SecurityExampleApplicationTests.java
+++ src/test/java/br/com/rts/estudos/graphql/security/SecurityExampleApplicationTests.java
@@ -1,13 +1,9 @@
 package br.com.rts.estudos.graphql.security;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SecurityExampleApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
