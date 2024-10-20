6f4eff6f55b6183aa4b82e0082606c3c71811fe1
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2024-10-20 11:28:34 -0400

    feat: Method configured to allow only defined authorities

--- src/main/java/br/com/rts/estudos/graphql/security/coffee/CoffeeController.java
+++ src/main/java/br/com/rts/estudos/graphql/security/coffee/CoffeeController.java
@@ -5,6 +5,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -23,14 +25,16 @@ public class CoffeeController {
         return repository.findById(id);
     }
 
+    @Secured("ROLE_USER")
     @QueryMapping
     public List<Coffee> coffees() {
         log.info("Find all coffees!");
         return repository.findAll();
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @MutationMapping(value = "createCoffee")
-    public Coffee create(@Argument String name, SizeCoffeeEnum size) {
+    public Coffee createCoffee(@Argument String name, @Argument SizeCoffeeEnum size) {
         return repository
                 .save(Coffee
                         .builder()
